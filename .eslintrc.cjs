7aa707191bc709b1fc90c837817348e791eaa207
Radik Abubjazov <abubjazov@mail.ru>
2023-06-05 22:54:18 +0300

    INITIAL COMMIT

---
+++ .eslintrc.cjs
@@ -0,0 +1,96 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier", "eslint-plugin-import"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+      },
+      extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      rules: {
+        quotes: ["error", "double"],
+        "linebreak-style": ["error", "unix"],
+        "no-empty-function": "warn",
+        "@typescript-eslint/no-empty-function": "warn",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/no-misused-promises": "error",
+        "prettier/prettier": "error",
+        "react/display-name": "off",
+        "react/prop-types": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "no-console": "warn",
+        "no-case-declarations": "warn",
+        "@typescript-eslint/unbound-method": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@typescript-eslint/restrict-template-expressions": "warn",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/restrict-plus-operands": "warn",
+        "@typescript-eslint/no-non-null-assertion": "warn",
+        "import/order": [
+          "error",
+          {
+            groups: ["external", "internal", "object"],
+            pathGroups: [
+              {
+                pattern: "react*",
+                group: "external",
+              },
+              {
+                pattern: "{.,...}**/*.tsx",
+                group: "internal",
+              },
+              {
+                pattern: "{..,..}/**/*.ts ",
+                group: "internal",
+              },
+              {
+                pattern: "{...,.}/**/*.svg",
+                group: "object",
+              },
+              {
+                pattern: "{....,}/**/*.css",
+                group: "object",
+              },
+            ],
+            warnOnUnassignedImports: true,
+            "newlines-between": "always",
+            alphabetize: {
+              order: "asc",
+              caseInsensitive: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
