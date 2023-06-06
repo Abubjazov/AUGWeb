4fbb20df9ab7a5953daf7215ce3ba173b00b52af
Radik Abubjazov <abubjazov@mail.ru>
2023-06-06 17:30:36 +0300

    dev: removed all unnecessary

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import App from "./App.tsx"
-import AppCopy from "./AppCopy.tsx"
-
-import viteLogo from "./assets/vite.svg"
 
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-
-    <AppCopy />
-
-    <img src={viteLogo} className="logo" alt="Vite logo" />
   </React.StrictMode>,
 )
