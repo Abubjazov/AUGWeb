ae27b815d84d4d7b8b4e89a0fe99e55e258cc45a
Radik Abubjazov <abubjazov@mail.ru>
2023-06-09 22:16:58 +0300

    dev: Storybook setup

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,13 +4,40 @@ import Button, { ButtonProps } from './Button'
 
 const meta: Meta<ButtonProps> = {
   component: Button,
+  title: 'Button',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Primary UI component for user interaction',
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      description: 'Button contents',
+    },
+    primary: {
+      description: 'Is this the principal call to action on the page?',
+    },
+    size: {
+      description: 'How large should the button be?',
+      defaultValue: 'medium',
+    },
+    backgroundColor: {
+      description: 'What background color to use?',
+    },
+    onClick: {
+      description: 'Optional click handler',
+    },
+  },
 }
 
 export default meta
 
 type Story = StoryObj<ButtonProps>
 
-export const DefaultButton: Story = {
+export const BaseButton: Story = {
   args: {
     label: 'Default Button',
   },
