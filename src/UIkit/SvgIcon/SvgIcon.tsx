4ab4828a8fadaffc3b9c7671dc6b032c0b5248a7
Radik Abubjazov <abubjazov@mail.ru>
2023-06-14 18:41:27 +0300

    dev: added MenuButton icons

--- src/UIkit/SvgIcon/SvgIcon.tsx
+++ src/UIkit/SvgIcon/SvgIcon.tsx
@@ -5,11 +5,30 @@ import { InstallButtonMode } from 'uikit/InstallButton/InstallButton'
 import { ReactComponent as InstallIcon } from './icons/InstallButton/install.svg'
 import { ReactComponent as InstalledIcon } from './icons/InstallButton/installed.svg'
 import { ReactComponent as UninstallIcon } from './icons/InstallButton/uninstall.svg'
-import { ReactComponent as RedCross } from './icons/redcross.svg'
-import { ReactComponent as SmarTagCross } from './icons/SmartTag/smarttagcross.svg'
+import { ReactComponent as LogoIcon } from './icons/logo.svg'
+import { ReactComponent as AllDappletsIcon } from './icons/MenuButton/alldapplets.svg'
+import { ReactComponent as EditorChoiceIcon } from './icons/MenuButton/editorchoice.svg'
+import { ReactComponent as EssentialDappletsIcon } from './icons/MenuButton/essentialdapplets.svg'
+import { ReactComponent as FinancialDappletsIcon } from './icons/MenuButton/financialdapplets.svg'
+import { ReactComponent as SocialNetworksIcon } from './icons/MenuButton/socialnetworks.svg'
+import { ReactComponent as RedCrossIcon } from './icons/redcross.svg'
+import { ReactComponent as SmarTagCrossIcon } from './icons/SmartTag/smarttagcross.svg'
+
+export enum MenuButtonIcon {
+  ALL_DAPPLETS = 'alldapplets',
+  ESSENTIAL_DAPPLETS = 'essentialdapplets',
+  FINANCIAL_DAPPLETS = 'financialdapplets',
+  EDITOR_CHOICE = 'editorchoice',
+  SOCIAL_NETWORKS = 'socialnetworks',
+}
 
 interface SvgIconProps {
-  icon: InstallButtonMode | 'smarttagcross' | 'redcross'
+  icon:
+    | InstallButtonMode
+    | MenuButtonIcon
+    | 'smarttagcross'
+    | 'redcross'
+    | 'logo'
 }
 
 const SvgIcon: FC<SvgIconProps> = ({ icon }) => {
@@ -26,12 +45,36 @@ const SvgIcon: FC<SvgIconProps> = ({ icon }) => {
       return <UninstallIcon />
     }
 
+    case MenuButtonIcon.ALL_DAPPLETS: {
+      return <AllDappletsIcon />
+    }
+
+    case MenuButtonIcon.ESSENTIAL_DAPPLETS: {
+      return <EssentialDappletsIcon />
+    }
+
+    case MenuButtonIcon.FINANCIAL_DAPPLETS: {
+      return <FinancialDappletsIcon />
+    }
+
+    case MenuButtonIcon.EDITOR_CHOICE: {
+      return <EditorChoiceIcon />
+    }
+
+    case MenuButtonIcon.SOCIAL_NETWORKS: {
+      return <SocialNetworksIcon />
+    }
+
     case 'smarttagcross': {
-      return <SmarTagCross />
+      return <SmarTagCrossIcon />
     }
 
     case 'redcross': {
-      return <RedCross />
+      return <RedCrossIcon />
+    }
+
+    case 'logo': {
+      return <LogoIcon />
     }
 
     default: {
