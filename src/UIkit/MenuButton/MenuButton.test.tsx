49f86841cd8b016de34faba2fea6479f88380d79
Radik Abubjazov <abubjazov@mail.ru>
2023-08-28 12:42:32 +0300

    dev: updated MenuButton tests

--- src/UIkit/MenuButton/MenuButton.test.tsx
+++ src/UIkit/MenuButton/MenuButton.test.tsx
@@ -1,25 +1,30 @@
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import MenuButton, { MenuButtonMode, MenuButtonIcon } from './MenuButton'
 
 describe('MenuButton', () => {
   test('should render MenuButton default', () => {
-    expect(
-      render(
-        <MenuButton text={'Menu button'} icon={MenuButtonIcon.ALL_DAPPLETS} />,
-      ),
-    ).toMatchSnapshot()
+    const { asFragment } = render(
+      <MenuButton
+        text={'Menu button'}
+        icon={MenuButtonIcon.ALL_DAPPLETS}
+        menuOpened={true}
+      />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
   })
 
   test('should render MenuButton active', () => {
-    expect(
-      render(
-        <MenuButton
-          text={'Menu button'}
-          icon={MenuButtonIcon.EDITOR_CHOICE}
-          mode={MenuButtonMode.ACTIVE}
-        />,
-      ),
-    ).toMatchSnapshot()
+    const { asFragment } = render(
+      <MenuButton
+        text={'Menu button'}
+        icon={MenuButtonIcon.EDITOR_CHOICE}
+        mode={MenuButtonMode.ACTIVE}
+        menuOpened={true}
+      />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
   })
 })
