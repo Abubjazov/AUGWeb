3b3aeed5d38388be4a8123433a6923984e88c12d
Radik Abubjazov <abubjazov@mail.ru>
2023-08-30 16:17:13 +0300

    dev: added DappletTags test

---
+++ src/components/DappletTags/DappletTags.test.tsx
@@ -0,0 +1,35 @@
+import { screen, fireEvent, render } from '@testing-library/react'
+import {
+  mockedReduxProvider as Provider,
+  store,
+} from 'mockData/mockedReduxProvider'
+
+import DappletTags from './DappletTags'
+
+describe('DappletTags', () => {
+  test('should render DappletTags default', () => {
+    const { asFragment } = render(
+      <Provider>
+        <DappletTags dappletId={1} dappletState={false} />
+      </Provider>,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should render DappletTags after removal some MyTag from dapplet', () => {
+    render(
+      <Provider>
+        <DappletTags dappletId={1} dappletState={false} />
+      </Provider>,
+    )
+
+    const myTagsState =
+      store.getState().myDapplets.myDapplets[0].userTags.length
+
+    fireEvent.click(screen.getAllByTestId('smart-tag-cross-button')[0])
+    expect(store.getState().myDapplets.myDapplets[0].userTags.length).toEqual(
+      myTagsState - 1,
+    )
+  })
+})
