89fc38ccd3ab4a839f4f555dc58b826b752ca664
Radik Abubjazov <abubjazov@mail.ru>
2023-11-04 18:13:58 +0300

    dev: refactored getTags

--- src/utils/getTags/getTags.ts
+++ src/utils/getTags/getTags.ts
@@ -1,64 +1,40 @@
 import { IDapplet, ITag } from 'store/slices/dappletsSlice'
 import { IUserDapplet } from 'store/slices/userDataSlice'
-import { ESmartTagMode } from 'uikit/SmartTag/SmartTag'
-
-// export const getTags = (
-//   dappletId: string,
-//   tags: ITag[],
-//   dapplets: IDapplet[] | IUserDapplet[],
-// ): IDapplet[] | IUserDapplet[] => {
-//   const targetDapplet = dapplets.filter(
-//     dapplet => dapplet.dappletId === dappletId,
-//   )[0]lf
-
-//   if (targetDapplet?.communityTags?.length) {
-//     const resTags: ITag[] = targetDapplet.communityTags.map(tagId => {
-//       const tagName = tags.filter(tag => tag.tagId === tagId)[0]?.tagName
-//       return { tagId, tagName }
-//     })
-
-//     return resTags
-//   }
-
-//   return []
-// }
 
 export const getTags = (
   dappletId: string,
-  tagMode: ESmartTagMode,
-  dapplets: IDapplet[],
-  userDapplets: IUserDapplet[],
   tags: ITag[],
-  userTags: ITag[],
-) => {
-  if (tagMode === ESmartTagMode.COMMUNITY_TAG) {
-    const targetDapplet = dapplets.filter(
-      dapplet => dapplet.dappletId === dappletId,
-    )[0]
+  dapplets: (IDapplet | IUserDapplet)[],
+): ITag[] => {
+  let targetDapplet: IDapplet | IUserDapplet = dapplets.filter(
+    dapplet => dapplet.dappletId === dappletId,
+  )[0]
 
-    if (targetDapplet?.communityTags?.length) {
-      const resTags: ITag[] = targetDapplet.communityTags.map(tagId => {
-        const tagName = tags.filter(tag => tag.tagId === tagId)[0]?.tagName
-        return { tagId, tagName }
-      })
+  let dappletTags: string[] = []
 
-      return resTags
-    }
+  if (
+    targetDapplet &&
+    Object.prototype.hasOwnProperty.call(targetDapplet, 'communityTags')
+  ) {
+    targetDapplet = targetDapplet as IDapplet
+    dappletTags = targetDapplet.communityTags
   }
 
-  if (tagMode === ESmartTagMode.MY_TAG) {
-    const targetDapplet = userDapplets.filter(
-      dapplet => dapplet.dappletId === dappletId,
-    )[0]
+  if (
+    targetDapplet &&
+    Object.prototype.hasOwnProperty.call(targetDapplet, 'userTags')
+  ) {
+    targetDapplet = targetDapplet as IUserDapplet
+    dappletTags = targetDapplet.userTags
+  }
 
-    if (targetDapplet?.userTags?.length) {
-      const resTags: ITag[] = targetDapplet.userTags.map(tagId => {
-        const tagName = userTags.filter(tag => tag.tagId === tagId)[0]?.tagName
-        return { tagId, tagName }
-      })
+  if (dappletTags.length) {
+    const result: ITag[] = dappletTags.map(tagId => {
+      const tagName = tags.filter(tag => tag.tagId === tagId)[0]?.tagName
+      return { tagId, tagName }
+    })
 
-      return resTags
-    }
+    return result
   }
 
   return []
