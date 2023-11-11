ebf118888f14a6439b8e8e32ddc9af802f348893
Radik Abubjazov <abubjazov@mail.ru>
2023-11-11 19:01:13 +0300

    dev: fixed SignInForm test

--- src/components/SingInForm/SingInForm.test.tsx
+++ src/components/SingInForm/SingInForm.test.tsx
@@ -1,16 +1,44 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { mockedReduxProvider as Provider } from 'mockData/mockedReduxProvider'
 
 import SingInForm from './SingInForm'
 
 describe('SingInForm', () => {
   test('should render SingInForm default', () => {
+    const mockFn = vi.fn()
+
     const { asFragment } = render(
       <Provider>
-        <SingInForm userFunction={() => {}} onSignIn={() => {}} />
+        <SingInForm userFunction={mockFn} onSignIn={mockFn} />
       </Provider>,
     )
 
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should call SignIn function', () => {
+    const mockFn = vi.fn()
+
+    render(
+      <Provider>
+        <SingInForm userFunction={mockFn} onSignIn={mockFn} />
+      </Provider>,
+    )
+
+    fireEvent.click(screen.getByText('Sign in'))
+
+    expect(mockFn).toHaveBeenCalledTimes(0)
+
+    fireEvent.change(screen.getByPlaceholderText('email@example.com'), {
+      target: { value: 'email@test.tst' },
+    })
+
+    fireEvent.change(screen.getByPlaceholderText('password'), {
+      target: { value: '12345678' },
+    })
+
+    fireEvent.click(screen.getByText('Sign in'))
+
+    expect(mockFn).toHaveBeenCalledTimes(1)
   })
 })
