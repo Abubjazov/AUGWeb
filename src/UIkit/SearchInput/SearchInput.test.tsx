3d5f83305bff6fe77581c9c22c5ecb9c158eb97b
Radik Abubjazov <abubjazov@mail.ru>
2023-11-12 19:47:10 +0300

    dev: fixed SearchInput test

--- src/UIkit/SearchInput/SearchInput.test.tsx
+++ src/UIkit/SearchInput/SearchInput.test.tsx
@@ -4,6 +4,12 @@ import * as actions from 'store/slices/dappletsSlice'
 
 import SearchInput from './SearchInput'
 
+function delay(timeout: number | undefined) {
+  return new Promise((resolve: (value: unknown) => void) => {
+    setTimeout(resolve, timeout)
+  })
+}
+
 describe('SearchInput', () => {
   test('should render SearchInput default', () => {
     const { asFragment } = render(
@@ -23,12 +29,6 @@ describe('SearchInput', () => {
         <SearchInput placeholder={'Placeholder'} />
       </Provider>,
     )
-
-    function delay(timeout: number | undefined) {
-      return new Promise((resolve: (value: unknown) => void) => {
-        setTimeout(resolve, timeout)
-      })
-    }
 
     expect(mockedSetSearchString).toHaveBeenCalledTimes(1)
     expect(mockedSetSearchString).toHaveBeenCalledWith('')
