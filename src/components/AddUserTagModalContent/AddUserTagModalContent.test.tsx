5b4691e6f580b7ffb85dd42fcec635cceb2578fe
Radik Abubjazov <abubjazov@mail.ru>
2023-11-12 21:19:41 +0300

    dev: refactored AddUserTagModalContent test

--- src/components/AddUserTagModalContent/AddUserTagModalContent.test.tsx
+++ src/components/AddUserTagModalContent/AddUserTagModalContent.test.tsx
@@ -1,16 +1,32 @@
-import { render } from '@testing-library/react'
-import { mockedReduxProvider as Provider } from 'mockData/mockedReduxProvider'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { mockedReduxProvider as MProvider } from 'mockData/mockedReduxProvider'
+import * as actions from 'store/slices/layoutSlice'
 
 import AddUserTagModalContent from './AddUserTagModalContent'
 
 describe('AddUserTagModalContent', () => {
   test('should render AddUserTagModalContent default', () => {
     const { asFragment } = render(
-      <Provider>
+      <MProvider>
         <AddUserTagModalContent dappletId={'ECNk2nNngwGXouvMpjWt'} />
-      </Provider>,
+      </MProvider>,
     )
 
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should call setModalState function on Close button click', () => {
+    const mockedSetModalState = vi.spyOn(actions, 'setModalState')
+
+    render(
+      <MProvider>
+        <AddUserTagModalContent dappletId={'ECNk2nNngwGXouvMpjWt'} />
+      </MProvider>,
+    )
+
+    fireEvent.click(screen.getByText('Close'))
+
+    expect(mockedSetModalState).toHaveBeenCalledTimes(1)
+    expect(mockedSetModalState).toHaveBeenCalledWith(false)
   })
 })
