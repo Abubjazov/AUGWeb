3acc19e5fd3058843809c634f0580aff7b358358
Radik Abubjazov <abubjazov@mail.ru>
2023-11-13 18:54:24 +0300

    dev: fixed SearchGroup test

--- src/components/SearchGroup/SearchGroup.test.tsx
+++ src/components/SearchGroup/SearchGroup.test.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react'
+import { mockedReduxProvider as Provider } from 'mockData/mockedReduxProvider'
 
 import SearchGroup from './SearchGroup'
 
 describe('SearchGroup', () => {
   test('should render SearchGroup default', () => {
-    const { asFragment } = render(<SearchGroup />)
+    const { asFragment } = render(
+      <Provider>
+        <SearchGroup />
+      </Provider>,
+    )
 
     expect(asFragment()).toMatchSnapshot()
   })
