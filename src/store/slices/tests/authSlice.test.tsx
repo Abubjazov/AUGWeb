57f1bdaa84d31cb75adaffd54b45d6699aa8523e
Radik Abubjazov <abubjazov@mail.ru>
2023-11-15 22:04:42 +0300

    dev: added auth asinkThunks tests

--- src/store/slices/tests/authSlice.test.tsx
+++ src/store/slices/tests/authSlice.test.tsx
@@ -1,22 +1,170 @@
-import { mokedStore } from 'mockData/mockedReduxProvider'
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import * as apiMethods from 'api/fireStore/fireStoreMethods'
+import { mockedStore } from 'mockData/mockedReduxProvider'
 
-import { setAuthData, setUserAuthenticated } from '../authSlice'
+import { createUser, logIn, logOut } from '../asyncThunks/authentication'
+import {
+  ISignUpData,
+  setAuthData,
+  setIsInProgress,
+  setUserAuthenticated,
+} from '../authSlice'
 
 describe('authSlice', () => {
-  test('reducer: setUserAuthenticated test', () => {
-    mokedStore.dispatch(setUserAuthenticated(true))
+  describe('reducers', () => {
+    test('reducer: setUserAuthenticated', () => {
+      mockedStore.dispatch(setUserAuthenticated(true))
+      expect(mockedStore.getState().auth.isUserAuthenticated).toBe(true)
 
-    expect(mokedStore.getState().auth.isUserAuthenticated).toBe(true)
+      mockedStore.dispatch(setUserAuthenticated(false))
+      expect(mockedStore.getState().auth.isUserAuthenticated).toBe(false)
+    })
 
-    mokedStore.dispatch(setUserAuthenticated(false))
+    test('reducer: setIsInProgress', () => {
+      mockedStore.dispatch(setIsInProgress(true))
+      expect(mockedStore.getState().auth.isInProgress).toBe(true)
 
-    expect(mokedStore.getState().auth.isUserAuthenticated).toBe(false)
+      mockedStore.dispatch(setIsInProgress(false))
+      expect(mockedStore.getState().auth.isInProgress).toBe(false)
+    })
+
+    test('reducer: setAuthData', () => {
+      mockedStore.dispatch(setAuthData({ uid: 'userid', email: 'email' }))
+
+      expect(mockedStore.getState().auth.email).toBe('email')
+      expect(mockedStore.getState().auth.uid).toBe('userid')
+    })
   })
 
-  test('reducer: setAuthData test', () => {
-    mokedStore.dispatch(setAuthData({ uid: 'userid', email: 'email' }))
+  describe('asyncThunk: createUser', () => {
+    const payloadData: ISignUpData = {
+      email: 'email@test.tst',
+      password: 'password',
+    }
 
-    expect(mokedStore.getState().auth.email).toBe('email')
-    expect(mokedStore.getState().auth.uid).toBe('userid')
+    const mockedApiCreateUser = vi
+      .spyOn(apiMethods, 'apiCreateUser')
+      .mockResolvedValueOnce()
+      .mockRejectedValueOnce(new Error('Async error'))
+
+    const dispatch = vi.fn()
+    const thunk = createUser(payloadData)
+
+    test('"resolved"', async () => {
+      await thunk(dispatch, () => ({}), undefined)
+
+      const [start, step1, step2, end] = dispatch.mock.calls.flat()
+
+      expect(start.type).toBe(createUser.pending.type)
+      expect(step1).toEqual({ type: 'auth/setIsInProgress', payload: true })
+      expect(step2).toEqual({ type: 'auth/setIsInProgress', payload: false })
+      expect(end.type).toBe(createUser.fulfilled.type)
+
+      expect(mockedApiCreateUser).toHaveBeenCalledTimes(1)
+      expect(mockedApiCreateUser).toHaveBeenCalledWith(payloadData)
+
+      dispatch.mockReset()
+    })
+
+    test('"rejected"', async () => {
+      await thunk(dispatch, () => ({}), undefined)
+
+      const [start, step1, error, step2, end] = dispatch.mock.calls.flat()
+
+      expect(start.type).toBe(createUser.pending.type)
+      expect(step1).toEqual({ type: 'auth/setIsInProgress', payload: true })
+      expect(error).toEqual({
+        type: 'layout/addMessage',
+        payload: { messageText: 'Async error', messageType: 'error' },
+      })
+      expect(step2).toEqual({ type: 'auth/setIsInProgress', payload: false })
+      expect(end.type).toBe(createUser.fulfilled.type)
+    })
+  })
+
+  describe('asyncThunk: logIn', () => {
+    const payloadData: ISignUpData = {
+      email: 'email@test.tst',
+      password: 'password',
+    }
+
+    const mockedApiLogIn = vi
+      .spyOn(apiMethods, 'apiLogIn')
+      .mockResolvedValueOnce()
+      .mockRejectedValueOnce(new Error('Async error'))
+
+    const dispatch = vi.fn()
+    const thunk = logIn(payloadData)
+
+    test('"resolved"', async () => {
+      await thunk(dispatch, () => ({}), undefined)
+
+      const [start, step1, step2, end] = dispatch.mock.calls.flat()
+
+      expect(start.type).toBe(logIn.pending.type)
+      expect(step1).toEqual({ type: 'auth/setIsInProgress', payload: true })
+      expect(step2).toEqual({ type: 'auth/setIsInProgress', payload: false })
+      expect(end.type).toBe(logIn.fulfilled.type)
+
+      expect(mockedApiLogIn).toHaveBeenCalledTimes(1)
+      expect(mockedApiLogIn).toHaveBeenCalledWith(payloadData)
+
+      dispatch.mockReset()
+    })
+
+    test('"rejected"', async () => {
+      await thunk(dispatch, () => ({}), undefined)
+
+      const [start, step1, error, step2, end] = dispatch.mock.calls.flat()
+
+      expect(start.type).toBe(logIn.pending.type)
+      expect(step1).toEqual({ type: 'auth/setIsInProgress', payload: true })
+      expect(error).toEqual({
+        type: 'layout/addMessage',
+        payload: { messageText: 'Async error', messageType: 'error' },
+      })
+      expect(step2).toEqual({ type: 'auth/setIsInProgress', payload: false })
+      expect(end.type).toBe(logIn.fulfilled.type)
+    })
+  })
+
+  describe('asyncThunk: logOut', () => {
+    const mockedApiLogOut = vi
+      .spyOn(apiMethods, 'apiLogOut')
+      .mockResolvedValueOnce()
+      .mockRejectedValueOnce(new Error('Async error'))
+
+    const dispatch = vi.fn()
+    const thunk = logOut()
+
+    test('"resolved"', async () => {
+      await thunk(dispatch, () => ({}), undefined)
+
+      const [start, step1, step2, end] = dispatch.mock.calls.flat()
+
+      expect(start.type).toBe(logOut.pending.type)
+      expect(step1).toEqual({ type: 'auth/setIsInProgress', payload: true })
+      expect(step2).toEqual({ type: 'auth/setIsInProgress', payload: false })
+      expect(end.type).toBe(logOut.fulfilled.type)
+
+      expect(mockedApiLogOut).toHaveBeenCalledTimes(1)
+
+      dispatch.mockReset()
+    })
+
+    test('"rejected"', async () => {
+      await thunk(dispatch, () => ({}), undefined)
+
+      const [start, step1, error, step2, end] = dispatch.mock.calls.flat()
+
+      expect(start.type).toBe(logOut.pending.type)
+      expect(step1).toEqual({ type: 'auth/setIsInProgress', payload: true })
+      expect(error).toEqual({
+        type: 'layout/addMessage',
+        payload: { messageText: 'Async error', messageType: 'error' },
+      })
+      expect(step2).toEqual({ type: 'auth/setIsInProgress', payload: false })
+      expect(end.type).toBe(logOut.fulfilled.type)
+    })
   })
 })
