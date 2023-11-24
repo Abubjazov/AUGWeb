9c484101390937c14c2aa4387be63d27aea9166f
Radik Abubjazov <abubjazov@mail.ru>
2023-11-25 00:20:19 +0300

    dev: added fireStoreGetCollection test

--- src/api/fireStore/fireStoreAPI.test.ts
+++ src/api/fireStore/fireStoreAPI.test.ts
@@ -1,25 +1,21 @@
 import {
-  DocumentData,
-  DocumentSnapshot,
-  SetOptions,
   doc,
   getDoc,
   getDocs,
-  getFirestore,
   setDoc,
   collection,
-  QuerySnapshot,
   query,
   limit,
   startAfter,
   where,
-  CollectionReference,
-  QueryLimitConstraint,
-  QueryFieldFilterConstraint,
-  QueryStartAtConstraint,
 } from 'firebase/firestore'
+import { ELastVisible, EQueryOperator } from 'store/slices/dappletsSlice'
 
-import { fireStoreGetDoc, fireStoreSetDoc } from './fireStoreAPI'
+import {
+  fireStoreGetCollection,
+  fireStoreGetDoc,
+  fireStoreSetDoc,
+} from './fireStoreAPI'
 
 vi.mock('firebase/firestore')
 
@@ -69,5 +65,73 @@ describe('fireStoreAPI', () => {
     expect(mockedDataConverter).toHaveBeenCalledWith(undefined)
 
     vi.resetAllMocks()
+  })
+
+  describe('fireStoreGetCollection', () => {
+    test('fireStoreGetCollection default call resolved', async () => {
+      const mockedCollection = vi.mocked(collection)
+      const mockedQuery = vi.mocked(query)
+      const mockedGetDocs = vi.mocked(getDocs)
+
+      const mockedDataConverter = vi.fn()
+
+      await fireStoreGetCollection('collectionName', mockedDataConverter)
+
+      expect(mockedCollection).toHaveBeenCalledTimes(1)
+      expect(mockedCollection).toHaveBeenCalledWith(undefined, 'collectionName')
+
+      expect(mockedQuery).toHaveBeenCalledTimes(1)
+
+      expect(mockedGetDocs).toHaveBeenCalledTimes(1)
+
+      expect(mockedDataConverter).toHaveBeenCalledTimes(1)
+
+      vi.resetAllMocks()
+    })
+
+    test('fireStoreGetCollection call "withWhere & withLimit & withStartAfter" resolved', async () => {
+      const testWhere = {
+        field: 'fieldName',
+        operator: EQueryOperator.ARRAY_CONTAINS,
+        comparisonValue: 'value',
+      }
+
+      const mockedCollection = vi.mocked(collection)
+      const withLimit = vi.mocked(limit)
+      const withStartAfter = vi.mocked(startAfter)
+      const mockedWhere = vi.mocked(where)
+      const mockedQuery = vi.mocked(query)
+      const mockedGetDocs = vi.mocked(getDocs)
+
+      const mockedDataConverter = vi.fn()
+
+      await fireStoreGetCollection(
+        'collectionName',
+        mockedDataConverter,
+        13,
+        ELastVisible.NO_MORE_DAPPLETS,
+        testWhere,
+      )
+
+      expect(mockedCollection).toHaveBeenCalledTimes(1)
+      expect(mockedCollection).toHaveBeenCalledWith(undefined, 'collectionName')
+
+      expect(withLimit).toHaveBeenCalledTimes(1)
+      expect(withLimit).toHaveBeenCalledWith(13)
+
+      expect(withStartAfter).toHaveBeenCalledTimes(1)
+      expect(withStartAfter).toHaveBeenCalledWith(ELastVisible.NO_MORE_DAPPLETS)
+
+      expect(mockedWhere).toHaveBeenCalledTimes(1)
+      expect(mockedWhere).toHaveBeenCalledWith(...Object.values(testWhere))
+
+      expect(mockedQuery).toHaveBeenCalledTimes(1)
+
+      expect(mockedGetDocs).toHaveBeenCalledTimes(1)
+
+      expect(mockedDataConverter).toHaveBeenCalledTimes(1)
+
+      vi.resetAllMocks()
+    })
   })
 })
