053832f534d230860d56a9eb8473fbdf3a3c5a10
Radik Abubjazov <abubjazov@mail.ru>
2023-11-24 23:53:32 +0300

    dev: added fireStoreGetDoc test

--- src/api/fireStore/fireStoreAPI.test.ts
+++ src/api/fireStore/fireStoreAPI.test.ts
@@ -19,7 +19,7 @@ import {
   QueryStartAtConstraint,
 } from 'firebase/firestore'
 
-import { fireStoreSetDoc } from './fireStoreAPI'
+import { fireStoreGetDoc, fireStoreSetDoc } from './fireStoreAPI'
 
 vi.mock('firebase/firestore')
 
@@ -43,5 +43,31 @@ describe('fireStoreAPI', () => {
     expect(mockedSetDoc).toHaveBeenCalledWith(undefined, 'data', {
       merge: true,
     })
+
+    vi.resetAllMocks()
+  })
+
+  test('fireStoreGetDoc call resolved', async () => {
+    const mockedDoc = vi.mocked(doc)
+    const mockedGetDoc = vi.mocked(getDoc)
+
+    const mockedDataConverter = vi.fn()
+
+    await fireStoreGetDoc('collectionName', 'documentName', mockedDataConverter)
+
+    expect(mockedDoc).toHaveBeenCalledTimes(1)
+    expect(mockedDoc).toHaveBeenCalledWith(
+      undefined,
+      'collectionName',
+      'documentName',
+    )
+
+    expect(mockedGetDoc).toHaveBeenCalledTimes(1)
+    expect(mockedGetDoc).toHaveBeenCalledWith(undefined)
+
+    expect(mockedDataConverter).toHaveBeenCalledTimes(1)
+    expect(mockedDataConverter).toHaveBeenCalledWith(undefined)
+
+    vi.resetAllMocks()
   })
 })
