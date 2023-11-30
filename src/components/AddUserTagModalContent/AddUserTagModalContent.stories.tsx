d39ccfb78b28cc53f4b4cc0abf815cb8c26a71b8
Radik Abubjazov <abubjazov@mail.ru>
2023-11-30 15:24:41 +0300

    dev: added AddUserTagModalContent story

---
+++ src/components/AddUserTagModalContent/AddUserTagModalContent.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import MockedProvider from 'mockData/mockedReduxProvider'
+
+import AddUserTagModalContent, {
+  AddUserTagModalContentProps,
+} from './AddUserTagModalContent'
+
+import '/src/index.css'
+
+const meta: Meta<AddUserTagModalContentProps> = {
+  component: AddUserTagModalContent,
+  title: 'COMPONENTS/AddUserTagModalContent',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Interface component - AddUserTagModalContent',
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    dappletId: {
+      description: 'Current dapplet ID',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<AddUserTagModalContentProps>
+
+export const Default: Story = {
+  args: {
+    dappletId: 'ECNk2nNngwGXouvMpjWt',
+  },
+
+  decorators: [
+    Story => (
+      <MockedProvider>
+        <Story />
+      </MockedProvider>
+    ),
+  ],
+}
