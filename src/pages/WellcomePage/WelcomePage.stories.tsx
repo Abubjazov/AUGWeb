e4fb25a6bc588d91aed79d5d281c789709873e4f
Radik Abubjazov <abubjazov@mail.ru>
2023-11-30 14:56:26 +0300

    dev: added WelcomePage story

---
+++ src/pages/WellcomePage/WelcomePage.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import MockedProvider from 'mockData/mockedReduxProvider'
+import { BrowserRouter } from 'react-router-dom'
+
+import WelcomePage, { WelcomePageProps } from './WelcomePage'
+
+import '/src/index.css'
+
+const meta: Meta<WelcomePageProps> = {
+  component: WelcomePage,
+  title: 'PAGES/WelcomePage',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Page component - WelcomePage. Contains such components as: StandardModal with WelcomeModalContent, MessageArea',
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    NoProps: {
+      description: 'There are no props for this component',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<WelcomePageProps>
+
+export const Default: Story = {
+  args: {},
+  decorators: [
+    Story => (
+      <MockedProvider>
+        <BrowserRouter>
+          <Story />
+        </BrowserRouter>
+      </MockedProvider>
+    ),
+  ],
+}
