1feb24e4069392b8339927c0bd314372508054c3
Radik Abubjazov <abubjazov@mail.ru>
2023-11-30 19:25:17 +0300

    dev: added NotAvailable story

---
+++ src/UIkit/NotAvailable/NotAvailable.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import MockedProvider from 'mockData/mockedReduxProvider'
+
+import NotAvailable, { NotAvailableProps } from './NotAvailable'
+
+import '/src/index.css'
+
+const meta: Meta<NotAvailableProps> = {
+  component: NotAvailable,
+  title: 'UIkit/NotAvailable',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Standard interface component - NotAvailable',
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    text: {
+      description: 'Title text',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<NotAvailableProps>
+
+export const Default: Story = {
+  args: { text: 'Title text' },
+  decorators: [
+    Story => (
+      <MockedProvider>
+        <Story />
+      </MockedProvider>
+    ),
+  ],
+}
