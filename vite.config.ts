b34672c6c9ce86b159cb0bd1946f2ae292e0c97e
Radik Abubjazov <abubjazov@mail.ru>
2023-12-15 13:58:12 +0300

    dev: refactored to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,41 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/AUGWeb/',
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'AUGWeb',
+        short_name: 'AUGWeb',
+        description:
+          'We are building an Augmented Web platform consisting of a browser-basedplugin and decentralized applications (dapplets) based on cryptotechnologies.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '.',
+        scope: './AUGWeb/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
